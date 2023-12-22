83a87a39dd74f0f1cbe7150577a5a43f3626c3b0
kaechb <benno.kaech@hotmail.com>
2023-12-22 15:14:34 +0100

    fixed documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gen_model_playground',
-    version='0.1',
+    version='0.0.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     insall_requires=[
