d05e5900415f782b418fe4547cae55bf9e853486
kaechb <benno.kaech@hotmail.com>
2023-12-25 11:13:35 +0100

    almost fixed doc

--- gen_model_playground/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .twomoons import TwoMoonsDataModule
--- gen_model_playground/fit/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cnf import CNF
-from .ddpm import DDPM
-from .flow import Flow
-from .gan import GAN
-from .vae import VAE
-from .flow_matching import FM
